3f8f7b7f8427cca23a604107145d3737da0231fe
mahendrav9 <mahi.java9@gmail.com>
2018-02-01 17:37:56 +0530

    initial.....

--- src/test/java/Event/event.java
+++ src/test/java/Event/event.java
@@ -1,5 +1,34 @@
 package Event;
 
+import javax.xml.crypto.dsig.SignedInfo;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import Element.element;
+import Utility.Utility;
+
 public class event {
 
+	
+	element ele=new element();
+	 static  WebDriver driver;
+
+	@BeforeClass
+	
+	public void initElement(){
+		Utility.openBrowser();
+		Utility.setUrl();
+		PageFactory.initElements(driver, ele);
+	}
+	
+	
+	@Test
+	
+	public void firstTest(){
+		
+	}
 }
--- src/test/java/Utility/Utility.java
+++ src/test/java/Utility/Utility.java
@@ -9,9 +9,13 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class Utility {
 	
@@ -19,6 +23,7 @@ public class Utility {
 	
 	static ExtentReports report;
 	static ExtentTest test;
+	
 	
 	public void openBrowser(){
 		
@@ -48,8 +53,26 @@ public class Utility {
 	public void closebrws(){
 			d.close();}
 	
-	public void takescreenshot() throws IOException{
+	public String takescreenshot(WebDriver dr,String screenname) throws IOException{
+		String path=config.testdata+screenname+".png";
 		File ss=((TakesScreenshot)d).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(ss, new File(config.testdata+"ss"+".png"));
+		FileUtils.copyFile(ss, new File(path));
+		return path;
 	}
+	
+	@BeforeTest
+	
+	public static void generateReport(){
+		report=new ExtentReports("D:/Users/MyWorkspace/exe/src/test/java/report");
+	}
+
+	@AfterMethod
+	public void failedRepo(ITestResult result) throws Exception{
+		
+		if(result.getStatus()==ITestResult.FAILURE){
+			
+		String imagePath=takescreenshot( d,"failed");
+		test.log(LogStatus.FAIL, result.getThrowable());
+		test.log(LogStatus.FAIL, "failed here", test.addScreenCapture(imagePath));
+	}}
 }
